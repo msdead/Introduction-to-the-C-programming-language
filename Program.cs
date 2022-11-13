bdddb49e40f53e4ba62a2eb50e8fa827aa350e41
Sergey <manerov.s@nail.ru>
2022-11-13 05:10:16 +0300

    ДЗ задача 8 готова

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿        static void Main()
-        {
-            if (int.Parse(Console.ReadLine()) % 2 == 0)
-                Console.WriteLine("Это чётное число");
-            else Console.WriteLine("Это нечётное число");
-            Console.ReadKey();
-        }

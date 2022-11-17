56ff6fa998d59a7bca2c5eb46f8bd5d3ad8f552b
Sergey <manerov.s@nail.ru>
2022-11-17 16:16:58 +0300

    Задача 10 готова

---
+++ ДЗ №2/Задача 10/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 --> {number}");
+
+int result1 = number % 100;
+int result2 = result1 / 10;
+
+Console.WriteLine(result2);

6686e5cbb41a2d51d007422b021e78b4bdd2dd12
Sergey <manerov.s@nail.ru>
2022-11-17 21:28:23 +0300

    Изменение решения задачи 15 в соответствии с требованием данного преподавателем

--- ДЗ №2/Задача 15/Program.cs
+++ ДЗ №2/Задача 15/Program.cs
@@ -1,9 +1,11 @@
-﻿int holiday1 = 6;
+﻿int index = 0;
+int holiday1 = 6;
 int holiday2 = 7;
-
 Console.WriteLine("Введите число обозначающую день недели, от 1 до 7");
-int index = Convert.ToInt32(Console.ReadLine());
-
+while (!int.TryParse(Console.ReadLine(), out index) || index < 1 || index > 7)
+{
+Console.WriteLine("Введено неверное число. Введите число обозначающую день недели, от 1 до 7");
+}
 bool Holiday()
 {
     return index == holiday1 || index == holiday2;
--- ДЗ №2/задача 13/Program.cs
+++ ДЗ №2/задача 13/Program.cs
@@ -1,3 +1,21 @@
 ﻿int number = new Random().Next(1, 100000);
 Console.WriteLine($"Случайное число из отрезка 100-999 --> {number}");
 
+while (number == 0)
+{
+int result = number / 10
+    
+}
+
+
+int result1 = number / 10;
+int result2 = result1 / 10;
+int result3 = result2 / 10;
+int result4 = result3 / 10;
+int result5 = result4 / 10;
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
+Console.WriteLine(result4);
+Console.WriteLine(result5);

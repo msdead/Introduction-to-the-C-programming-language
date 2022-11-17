f770ee816b02839ddf2734d040b5b33d5ba45101
Sergey <manerov.s@nail.ru>
2022-11-17 23:53:16 +0300

    13 задача готова

---
+++ ДЗ №2/Задача 13/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(1, 100);
+Console.WriteLine($"Случайное число из отрезка --> {number}");
+if (number < 100)
+{
+   Console.WriteLine("Третьего числа нет"); 
+   return;
+}
+while (number >= 1000)
+{
+ number = number / 10;
+}
+int result = number % 10;
+Console.WriteLine(result);

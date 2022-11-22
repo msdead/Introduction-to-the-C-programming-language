929fe7a92d438e4c5e67d896718143d4ae906154
Sergey <manerov.s@nail.ru>
2022-11-22 21:08:00 +0300

    Задача 23 готова

--- ДЗ №3/Задача 23/Program.cs
+++ ДЗ №3/Задача 23/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void Square(int x)
+{
+    for (int i = 1; i <= x; i++)
+        {
+            Console.WriteLine($"{i}^3 = {i * i * i}");
+        }
+}
+Square(x);

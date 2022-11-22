a67703945e49c28d6b00107e6a498bec77824005
Sergey <manerov.s@nail.ru>
2022-11-23 02:02:25 +0300

    Задача 27 готова

--- ДЗ №4/Задача 27/Program.cs
+++ ДЗ №4/Задача 27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int x)
+{
+    int result = 0;
+    while (x > 0)
+    {
+        result = result + x % 10; 
+        x = x / 10;  
+    }
+    return result;
+}
+Console.Write(Sum(x));

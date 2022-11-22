a9a96b1cb76597ee4d66d513b39bbbac3c391615
Sergey <manerov.s@nail.ru>
2022-11-22 17:03:03 +0300

    Изменение задачи 13 в дз №2

--- ДЗ №2/Задача 13/Program.cs
+++ ДЗ №2/Задача 13/Program.cs
@@ -1,13 +1,17 @@
-﻿int number = new Random().Next(1, 100);
+﻿int number = new Random().Next(-1000, 1000);
 Console.WriteLine($"Случайное число из отрезка --> {number}");
-if (number < 100)
+if (-100 < number & number < 100)
 {
    Console.WriteLine("Третьего числа нет"); 
    return;
 }
 while (number >= 1000)
 {
- number = number / 10;
+   number = number / 10;
+}
+if (number < 0)
+{
+   number = number * -1;
 }
 int result = number % 10;
-Console.WriteLine(result);
+Console.WriteLine($"Третья цифра числа --> {result}");

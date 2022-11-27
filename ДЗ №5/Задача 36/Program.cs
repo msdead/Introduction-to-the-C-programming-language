87d8ce84b85cb2d4a91390ac56f9b9d79606fb0e
Sergey <manerov.s@nail.ru>
2022-11-27 20:10:16 +0300

    ДЗ №5 готово

---
+++ ДЗ №5/Задача 36/Program.cs
@@ -0,0 +1,35 @@
+﻿int [] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd =new Random();
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");    
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumNegativeElem(int [] arr)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (i % 2 == 1) sumNegative += arr[i]; 
+    return sumNegative;
+}
+
+int[] array = CreateArrayRndInt(10, -100, 100);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов стоящих на нечётных позициях индекса = {GetSumNegativeElem(array)}");
+
---
+++ ДЗ №5/Задача 38/Program.cs
@@ -0,0 +1,51 @@
+﻿double [] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd =new Random();
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        
+    }
+    return arr;
+}
+
+void PrintArray (double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");    
+    }
+    Console.WriteLine("]");
+}
+
+double GetMin (double [] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] < min) min = arr[i]; 
+    return min;
+}
+
+double GetMax(double [] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] > max) max = arr[i];
+    return max;
+}
+
+double Difference(double max ,double min)
+{
+    double Difference = max - min;
+    return Math.Round(Difference, 1);
+}
+
+double[] array = CreateArrayRndDouble(5, -100, 100);
+PrintArray(array);
+double min = GetMin(array);
+double max = GetMax(array);
+Console.WriteLine($" Разница между максимальным и минимальным элементами массива = {Difference(max, min)}");

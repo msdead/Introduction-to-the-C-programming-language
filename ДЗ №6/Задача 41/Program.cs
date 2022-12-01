a758604a0e881259e1a88f70d89fe48159e4dc0e
Sergey <manerov.s@nail.ru>
2022-12-02 01:28:05 +0300

    ДЗ №6 готово

---
+++ ДЗ №6/Задача 41/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreateArrayRndInt(int size) // Метод создания массива
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindPositiveNumb(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write($"Введите какое количество чисел хотите использовать: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] arr = CreateArrayRndInt(size);
+FindPositiveNumb(arr);
+Console.WriteLine($"Количество ведённых чисел больше 0 : {FindPositiveNumb(arr)}");
---
+++ ДЗ №6/Задача 43/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double XIntersection(double b1, double k1, double b2, double k2)
+{
+        double xIntersection = (b2 - b1) / (k1 - k2);
+        return xIntersection;
+}
+
+double YIntersection(double b1, double k1, double xIntersection)
+{
+        double yIntersection = k1 * xIntersection + b1;
+        return yIntersection;
+}
+
+double xIntersection = XIntersection(b1,k1,b2,k2);
+double yIntersection = YIntersection(b1,k1,xIntersection);
+Console.WriteLine($"точка пересечения  ({xIntersection} ; {yIntersection})");

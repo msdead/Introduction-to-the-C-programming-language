79b37913a45683c0d187262362117699072955db
Sergey <manerov.s@nail.ru>
2022-12-01 16:42:51 +0300

    Семинар 6

---
+++ Семинар 6/4 Часть/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] Fibonacci(int num)
+{
+int[] arr = new int[num];
+arr[1] = 1;
+for (int i = 2; i < num; i++)
+{
+arr[i]=arr[i-1] + arr[i-2];
+}
+return arr;
+}
+
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+else Console.Write($"{array[i]}");
+}
+Console.WriteLine("]");
+}
+
+int [] resultArray = Fibonacci(number);
+Console.WriteLine($"Первые {number} чисел Фибоначчи:");
+PrintArray(resultArray);
